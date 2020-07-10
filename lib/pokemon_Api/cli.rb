2a58de4efbadad7b28f58fbc4d25c333ab17d0e3
Mitzi17 <m_estrada17@live.com>
2020-07-10 15:12:53 -0700

    define display_pokemon_names method

--- lib/pokemon_Api/cli.rb
+++ lib/pokemon_Api/cli.rb
@@ -11,7 +11,16 @@ module PokemonApi
             puts "Please select a number from the list below to get more options about that Pokemon."
             puts "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
 
-            API.get_pokemon     
+            API.get_pokemon
+            display_pokemon_names
+            
+            
+        end
+
+        def display_pokemon_names
+            Pokemon.all.each.with_index(1) do |pokemon_obj, idx|
+                puts "#{idx}. #{pokemon_obj.name.capitalize}"
+            end
         end
 
         
