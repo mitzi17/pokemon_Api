cc0dd34e3353fe3d5235b88ef955b0ec47846afe
Mitzi17 <m_estrada17@live.com>
2020-07-09 23:08:38 -0700

    define initialize and add attributes

--- lib/pokemon_Api/pokemon.rb
+++ lib/pokemon_Api/pokemon.rb
@@ -1,5 +1,22 @@
 module PokemonApi
     class Pokemon
 
+        attr_accessor :name, :url
+        @@all = [ ]
+
+        def initialize(hash)
+            @name = hash["name"]
+            @url = hash["url"]
+            save
+        end
+
+        def save
+            @@all << self
+        end
+
+        def self.all
+            @@all
+        end
+
     end
 end
