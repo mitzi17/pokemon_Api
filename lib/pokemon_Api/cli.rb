eb1b3eda8b1971e5682f14f60ed2162a1c8c729b
Mitzi17 <m_estrada17@live.com>
2020-07-09 20:10:18 -0700

    define start and get_pokemon_list methods

--- lib/pokemon_Api/cli.rb
+++ lib/pokemon_Api/cli.rb
@@ -1,7 +1,23 @@
 module PokemonApi
     class CLI
         def start
-            puts "hello its working!!"
+            puts ""
+            puts "*************************************"
+            puts "HELLO WELCOME TO THE ONLINE POKEDEX!!"
+            puts "*************************************"
+            puts ""
+            puts ""
+            sleep(2)
+            puts "Please select a number from the list below to get more options about that Pokemon."
+            puts "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "
+
+            get_pokemon_list
+        end
+
+        def get_pokemon_list
+            API.all.collet |pokemon_obj|
+                puts "pokemon.name"
+            end
         end
     end
 end
