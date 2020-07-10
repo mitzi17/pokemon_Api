c85d1858222bb2a8d27da78891e7e48abbe05857
Mitzi17 <m_estrada17@live.com>
2020-07-09 23:10:31 -0700

    define constant and class method get_pokemon to parse results

--- lib/pokemon_Api/api.rb
+++ lib/pokemon_Api/api.rb
@@ -1,5 +1,17 @@
+require 'pry'
 module PokemonApi
     class API
+        BASE_URL = 'https://pokeapi.co/api/v2/pokemon/'
 
+        def self.get_pokemon
+            results = RestClient.get(BASE_URL)
+            
+            hash_pokemons = JSON.parse(results)
+            hash_pokemons["results"].each do |hash|
+                Pokemon.new(hash)
+            binding.pry
+            end
+        end
+    
     end
 end
