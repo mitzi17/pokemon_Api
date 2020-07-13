af24176993d6b3373e0e05eb4c541f90de0690b9
Mitzi17 <m_estrada17@live.com>
2020-07-12 23:55:57 -0700

    define get_moves and get_types methods

--- lib/pokemon_Api/cli.rb
+++ lib/pokemon_Api/cli.rb
@@ -26,6 +26,8 @@ module PokemonApi
             API.query_pokemon(@pokemon_obj)
 
             display_abilities(@pokemon_obj)
+            display_moves(@pokemon_obj)
+            display_types(@pokemon_obj)
             binding.pry            
         end
 
@@ -39,6 +41,18 @@ module PokemonApi
         def display_abilities(pokemon_obj)
             @pokemon_obj.abilities.each.with_index(1) do |ability, idx|
                 puts "#{idx}. #{ability.capitalize}"
+            end
+        end
+
+        def display_moves(pokemon_obj)
+            @pokemon_obj.moves.each.with_index(1) do |move, idx|
+                puts "#{idx}. #{move.capitalize}"
+            end
+        end
+
+        def display_types(pokemon_obj)
+            @pokemon_obj.types.each.with_index(1) do |type, idx|
+                puts "#{idx}. #{type.capitalize}"
             end
         end
 
